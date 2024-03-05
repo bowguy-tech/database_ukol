16ddb0befe291d8a7c71389c84a896b4a3519eea
Maxwell Olsbro <68139501+bowguy-tech@users.noreply.github.com>
2024-03-05 15:58:07 +0100

    zakladni slozky

---
+++ databazovy_projekt/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace databazovy_projekt
+{
+    internal class Database
+    {
+        public Database(string credentials) {
+            SqlConnection conn;
+        }
+
+        public void Close()
+        {
+
+        }
+    }
+}
---
+++ databazovy_projekt/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace databazovy_projekt
+{
+
+    class SelectStatement
+    {
+
+        static void Main()
+        {
+            Stuff();
+            Console.ReadKey();
+        }
+
+        static void Stuff()
+        {
+            string connstr = @"Data Source=193.85.203.188;Initial Catalog=kremlik;User ID=kremlik;Password=pzoMwfFn_22";
+            conn = new SqlConnection(constr);
+            
+            conn.Open();
+
+            string sql = "select cislo from cool_table";
+
+            SqlCommand com = new SqlCommand(sql, conn);
+            SqlDataReader dataReader = com.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                Console.WriteLine(dataReader.GetString(0));
+            }
+            Console.WriteLine("reading complete");
+            conn.Close();
+        }
+    }
+}
